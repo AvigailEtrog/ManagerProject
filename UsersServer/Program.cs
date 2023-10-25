943e4ff6c38f0e303950fa4c815d29ac7eab1cd9
AvigailEtrog <Ae0527627762@gmail.com>
2023-10-25 21:01:15 +0300

    useInsertProductMethod

--- UsersServer/Program.cs
+++ UsersServer/Program.cs
@@ -9,8 +9,8 @@ static void Main(string[] args)
     {
         //int rowsAffected=d.InsertCategory("Data Source=SRV2\\PUPILS;Initial Catalog=AdoNetUsers;Integrated Security=True");
         //Console.WriteLine(rowsAffected +" rows affacted");
-        int rowsAffected = d.InsertProduct("Data Source=SRV2\\PUPILS;Initial Catalog=AdoNetUsers;Integrated Security=True");
-        Console.WriteLine(rowsAffected + " rows affacted");
+        //int rowsAffected = d.InsertProduct("Data Source=SRV2\\PUPILS;Initial Catalog=AdoNetUsers;Integrated Security=True");
+        //Console.WriteLine(rowsAffected + " rows affacted");
         d.readData("Data Source=SRV2\\PUPILS;Initial Catalog=AdoNetUsers;Integrated Security=True");
     }
 
